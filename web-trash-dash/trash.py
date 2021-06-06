0f7b999b322bbcaf4f20a698ceaffbca3a5ddefb
sevenbitscience <36937340+sevenbitscience@users.noreply.github.com>
2021-06-06 14:32:56 -0400

    Add player and trash as seperate files

---
+++ web-trash-dash/trash.py
@@ -0,0 +1,21 @@
+import pygame
+import random
+
+
+class Trash:
+    trashSprites = [pygame.image.load("assets/gfx/appleCore.png"), pygame.image.load("assets/gfx/soda.png")]
+
+    def __init__(self, screenheight):
+        self.screenheight = screenheight
+        self.size = random.randrange(20, 30)
+        self.sprite = self.trashSprites[random.randint(0, len(self.trashSprites)-1)]
+        self.sprite = pygame.transform.scale(self.sprite, (self.size, self.size))
+        self.sprite = pygame.transform.rotate(self.sprite, random.randrange(0, 360))
+        self.position = [random.randrange(370, 1100), random.randrange(-500, -50)]
+        self.speed = random.randrange(5, 10) / 10
+
+    def fall(self):
+        if self.position[1] < self.screenheight:
+            self.position[1] += self.speed
+        else:
+            self.__init__(self.screenheight)
