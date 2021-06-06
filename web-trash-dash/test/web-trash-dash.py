49fe25278400626bf2cab97b2d63011ab1c76d81
sevenbitscience <36937340+sevenbitscience@users.noreply.github.com>
2021-06-06 13:24:42 -0400

    Move title screen image

--- web-trash-dash/test/web-trash-dash.py
+++ web-trash-dash/test/web-trash-dash.py
@@ -1,6 +1,10 @@
 import pygame
 
 
+def check_collision_list(a, b):
+    return (a[0] + a[2] > b[0]) and (a[0] < b[0] + b[2]) and (a[1] + a[3] > b[1]) and (a[1] < b[1] + b[3])
+
+
 def main():
     # start pygame
     pygame.init()
@@ -8,6 +12,13 @@ def main():
     screen = pygame.display.set_mode((1280, 640))
     # pygame.display.set_caption("Trash dash")
 
+    title_screen = pygame.image.load("assets/gfx/TitleScreen.png")
+    title_screen = pygame.transform.scale(title_screen, (1280, 640))
+
+    begin_button = (328, 191, 746, 72)
+    screen.blit(title_screen, (0, 0))
+    # pygame.draw.rect(screen, (0, 0, 0), begin_button)
+    pygame.display.update()
     on_title = True
 
     while on_title:
@@ -17,10 +28,14 @@ def main():
             if event.type == pygame.QUIT:
                 pygame.quit()
                 break
-            if event.type == pygame.KEYDOWN and event.key == pygame.K_SPACE:
+            if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
+                select = True
+        if select:
+            mouse_pos = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 3, 3)
+            if check_collision_list(mouse_pos, begin_button):
+                # select_sound.play()
                 pygame.quit()
                 break
-        pygame.display.update()
 
 
 if __name__ == "__main__":
