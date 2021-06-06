bfc727554fbb14f1a34435f1cc9e9337b6570372
sevenbitscience <36937340+sevenbitscience@users.noreply.github.com>
2021-06-06 14:00:45 -0400

    Added the rest of the images

--- web-trash-dash/test/web-trash-dash.py
+++ web-trash-dash/test/web-trash-dash.py
@@ -1,4 +1,85 @@
 import pygame
+import random
+
+
+class Player:
+    position = pygame.Vector2()
+    position.xy = 100, 400
+    velocity = pygame.Vector2()
+    velocity.xy = 0, 0
+    speed = 2
+    # drag = 0.1
+    frame = 0
+    costume = 0
+    walk = [pygame.image.load("assets/gfx/walking/Dino R1.png"), pygame.image.load("assets/gfx/walking/Dino R2.png"),
+            pygame.image.load("assets/gfx/walking/Dino R3.png"), pygame.image.load("assets/gfx/walking/Dino R4.png"),
+            pygame.image.load("assets/gfx/walking/Dino R5.png"), pygame.image.load("assets/gfx/walking/Dino R6.png")]
+    idle = pygame.image.load("assets/gfx/dino.png")
+    duck_walk = [pygame.image.load("assets/gfx/DuckDino/Duck R1.png"), pygame.image.load(
+        "assets/gfx/DuckDino/Duck R2.png"),
+                 pygame.image.load("assets/gfx/DuckDino/Duck R3.png"), pygame.image.load(
+            "assets/gfx/DuckDino/Duck R4.png"),
+                 pygame.image.load("assets/gfx/DuckDino/Duck R5.png"), pygame.image.load(
+            "assets/gfx/DuckDino/Duck R6.png")]
+    duck_idle = pygame.image.load("assets/gfx/DuckDino/Duck Idle.png")
+    robo_walk = [pygame.image.load("assets/gfx/RoboDino/ROBO R1.png"), pygame.image.load(
+        "assets/gfx/RoboDino/ROBO R2.png"),
+                 pygame.image.load("assets/gfx/RoboDino/ROBO R3.png"), pygame.image.load(
+            "assets/gfx/RoboDino/ROBO R4.png"),
+                 pygame.image.load("assets/gfx/RoboDino/ROBO R5.png"), pygame.image.load(
+            "assets/gfx/RoboDino/ROBO R6.png")]
+    robo_idle = pygame.image.load("assets/gfx/RoboDino/Dino Bot Idle.png")
+    rightSprite = idle
+    rightSprite = pygame.transform.scale(rightSprite, (60, 60))
+    leftSprite = pygame.transform.flip(rightSprite, True, False)
+    currentSprite = rightSprite
+
+    def animate(self):
+        if self.frame >= len(self.walk):
+            self.frame = 0
+
+        if self.costume == 0:
+            self.rightSprite = self.walk[self.frame]
+        if self.costume == 1:
+            self.rightSprite = self.robo_walk[self.frame]
+        elif self.costume == 2:
+            self.rightSprite = self.duck_walk[self.frame]
+
+        self.rightSprite = pygame.transform.scale(self.rightSprite, (60, 60))
+        self.leftSprite = pygame.transform.flip(self.rightSprite, True, False)
+        self.frame += 1
+
+    def reset(self):
+        if self.costume == 0:
+            self.rightSprite = self.idle
+        if self.costume == 1:
+            self.rightSprite = self.robo_idle
+        elif self.costume == 2:
+            self.rightSprite = self.duck_idle
+
+        self.rightSprite = pygame.transform.scale(self.rightSprite, (60, 60))
+        self.leftSprite = pygame.transform.flip(self.rightSprite, True, False)
+
+
+
+class Trash:
+    trashSprites = [pygame.image.load("assets/gfx/appleCore.png"), pygame.image.load("assets/gfx/soda.png")]
+
+    def __init__(self, screenheight):
+        self.screenheight = screenheight
+        self.size = random.randrange(20, 30)
+        self.sprite = self.trashSprites[random.randint(0, len(self.trashSprites)-1)]
+        self.sprite = pygame.transform.scale(self.sprite, (self.size, self.size))
+        self.sprite = pygame.transform.rotate(self.sprite, random.randrange(0, 360))
+        self.position = pygame.Vector2()
+        self.position.xy = random.randrange(370, 1100), random.randrange(-500, -50)
+        self.speed = random.randrange(5, 10) / 10
+
+    def fall(self):
+        if self.position.y < self.screenheight:
+            self.position.y += self.speed
+        else:
+            self.__init__(self.screenheight)
 
 
 def check_collision_list(a, b):
@@ -14,6 +95,16 @@ def main():
 
     title_screen = pygame.image.load("assets/gfx/TitleScreen.png")
     title_screen = pygame.transform.scale(title_screen, (1280, 640))
+
+    hud_icon_size = 60
+    trash_pile = pygame.image.load("assets/gfx/soda.png")
+    trash_pile = pygame.transform.scale(trash_pile, (hud_icon_size, hud_icon_size))
+    coin = pygame.image.load("assets/gfx/coin.png")
+    coin = pygame.transform.scale(coin, (hud_icon_size, hud_icon_size))
+    timer_icon = pygame.image.load("assets/gfx/hourglass-icon.png")
+    timer_icon = pygame.transform.scale(timer_icon, (hud_icon_size, hud_icon_size))
+    backpack_icon = pygame.image.load("assets/gfx/backpack-icon.png")
+    backpack_icon = pygame.transform.scale(backpack_icon, (hud_icon_size, hud_icon_size))
 
     score_font = pygame.font.Font("assets/Fonts/Press_Start_2P/PressStart2P-Regular.ttf", 50)
     score_color = (191, 69, 69)
@@ -72,74 +163,59 @@ def main():
             mouse_pos = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 3, 3)
             if check_collision_list(mouse_pos, begin_button):
                 # select_sound.play()
-                pygame.quit()
-                break
+                on_title = False
 
     while True:
         while not running:
+            clock.tick(10)
             select = False
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
-                    break
+                    return
                 if event.type == pygame.MOUSEBUTTONDOWN and event.button == 1:
                     select = True
 
             if select:
                 mouse_pos = (pygame.mouse.get_pos()[0], pygame.mouse.get_pos()[1], 3, 3)
                 if check_collision_list(mouse_pos, start_button):
-                    # select_sound.play()
                     running = True
                 elif check_collision_list(mouse_pos, quit_button):
-                    select_sound.play()
                     pygame.time.wait(int(select_sound.get_length()*1000))
+                    pygame.quit()
                     return
                 elif check_collision_list(mouse_pos, speed_button):
                     if dino.speed < 10 and balance >= dino.speed * 20:
-                        # select_sound.play()
                         balance -= dino.speed * 20
                         dino.speed += 1
                         score_text = score_font.render(str(balance), True, score_color)
-                    else:
-                        full_sound.play()
                 elif check_collision_list(mouse_pos, backpack_button):
                     if backpack < 30 and balance >= backpack * 3:
-                        # select_sound.play()
                         balance -= backpack * 3
                         backpack += 5
                         score_text = score_font.render(str(balance), True, score_color)
                         backpack_text = score_font.render(str(backpack), True, score_color)
-                    else:
-                        full_sound.play()
                 elif check_collision_list(mouse_pos, atm_button):
                     if trash_price < 20 and balance >= trash_price * 10:
-                        # select_sound.play()
                         balance -= trash_price * 10
                         trash_price += 2
                         score_text = score_font.render(str(balance), True, score_color)
-                    else:
-                        full_sound.play()
                 elif check_collision_list(mouse_pos, costume1):
-                    # select_sound.play()
                     selected_icon = (94, 71, 13, 13)
                     dino.costume = 0
                     dino.reset()
                     pygame.display.set_icon(dino.rightSprite)
                 elif check_collision_list(mouse_pos, costume2):
-                    # select_sound.play()
                     selected_icon = (215, 71, 13, 13)
                     dino.costume = 1
                     dino.reset()
                     pygame.display.set_icon(dino.rightSprite)
                 elif check_collision_list(mouse_pos, costume3):
-                    # select_sound.play()
                     selected_icon = (329, 71, 13, 13)
                     dino.costume = 2
                     dino.reset()
                     pygame.display.set_icon(dino.rightSprite)
                 elif check_collision_list(mouse_pos, winButton):
-                    if GameWon or balance >= 1000:
-                        # select_sound.play()
                         GameWon = True
                         pygame.time.delay(10)
                         screen.blit(winScreen, (0, 0))
@@ -147,8 +223,6 @@ def main():
                         pygame.time.delay(50)
                         if not GameWon:
                             balance -= 1000
-                    else:
-                        full_sound.play()
 
             screen.blit(inside, (0, 0))
             pygame.draw.rect(screen, (13, 219, 67), selected_icon)
@@ -174,7 +248,6 @@ def main():
             screen.blit(backpack_icon, (450, 560))
             screen.blit(backpack_text, (510, 567))
 
-            clock.tick(10)
             pygame.display.update()
 
 
